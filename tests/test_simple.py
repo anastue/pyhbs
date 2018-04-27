c60bc5ba3f61c37f5466859a1911fbef79ca84c0
Anas Tue <anas.tue@gmail.com>
2018-04-27 15:52:18 +0700

    Fix : simple test

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,12 +1,11 @@
 from unittest import TestCase
 
 class TestSimple(TestCase):
-
     def test_import(self):
         import pyhbs
         pyhbs.render_source
         pyhbs.render_file
         pyhbs.register_helper
 
-    def test_success():
-        assert True
+def test_success():
+    assert True
